004bfeb9ab4be4dfa7a6d3c6ee6681319923c2e8
darklord <jet_wong@hotmail.com>
2014-10-09 12:11:15 +0800

    Completed the test for binarySearch

--- src/BinarySearch.c
+++ src/BinarySearch.c
@@ -6,12 +6,14 @@ int binarySearch(int target, int array[], int startIndex, int endIndex)
 {
 	int half_index;
 	half_index = (endIndex + startIndex)/2;
-
-	
+		
 	if( target == array[half_index] )
 		return half_index;
+	else if( startIndex == endIndex)
+		return -1;
 	else if( target > array[half_index])
 		half_index = binarySearch(target, array, half_index+1, endIndex);
 	else 
 		half_index = binarySearch(target, array, startIndex, half_index-1);
+		
 }
--- test/test_BinarySearch.c
+++ test/test_BinarySearch.c
@@ -9,6 +9,10 @@ void tearDown(void)
 {
 }
 
+/*
+ *	[3]	4	5
+ *
+ */
 void test_binarySearch_given_array_3_elements_with_target_3_should_return_0(void)
 {
 	int array[] = {3,4,5};
@@ -20,6 +24,10 @@ void test_binarySearch_given_array_3_elements_with_target_3_should_return_0(void
 }
 
 
+/*
+ *	3	4	[5]
+ *
+ */
 void test_binarySearch_given_array_3_elements_with_target_5_should_return_2(void)
 {
 	int array[] = {3,4,5};
@@ -31,47 +39,78 @@ void test_binarySearch_given_array_3_elements_with_target_5_should_return_2(void
 }
 
 
-void test_binarySearch_given_array_4_elements_with_target_5_should_return_0(void)
+/*
+ *	3	[4]	 5
+ *
+ */
+void test_binarySearch_given_array_3_elements_with_target_4_should_return_1(void)
 {
-	int array[] = {3,4,5,6};
+	int array[] = {3,4,5};
 	int result;
 	
-	result = binarySearch(5, array, 0, 3);
-	TEST_ASSERT_EQUAL(2, result);
-	TEST_ASSERT_EQUAL(5, array[result]);
-}
-
-
-void test_binarySearch_given_array_4_elements_with_target_3_should_return_0(void)
-{
-	int array[] = {3,4,5,6};
-	int result;
-	
-	result = binarySearch(3, array, 0, 3);
-	TEST_ASSERT_EQUAL(0, result);
-	TEST_ASSERT_EQUAL(3, array[result]);
-}
-
-
-void test_binarySearch_given_array_7_elements_with_target_10_should_return_4(void)
-{
-	int array[] = {2,4,6,8,10,12,14};
-	int result;
-	
-	result = binarySearch(10, array, 0, 6);
-	TEST_ASSERT_EQUAL(4, result);
-	TEST_ASSERT_EQUAL(10, array[result]);
-
-}
-
-
-void test_binarySearch_given_array_7_elements_with_target_4_should_return_4(void)
-{
-	int array[] = {2,4,6,8,10,12,14};
-	int result;
-	
-	result = binarySearch(4, array, 0, 6);
+	result = binarySearch(4, array, 0, 2);
 	TEST_ASSERT_EQUAL(1, result);
 	TEST_ASSERT_EQUAL(4, array[result]);
+}
+
+
+/*
+ *	3	4	 5			[10]
+ *
+ */
+void test_binarySearch_given_array_3_elements_with_target_10_should_return_neg_1(void)
+{
+	int array[] = {3,4,5};
+	int result;
+	
+	result = binarySearch(10, array, 0, 2);
+	TEST_ASSERT_EQUAL(-1, result);
 
 }
+
+
+/*
+ *	2	4	6	8	10	12	[14]
+ *
+ */
+void test_binarySearch_given_array_7_elements_with_target_14_should_return_4(void)
+{
+	int array[] = {2,4,6,8,10,12,14};
+	int result;
+	
+	result = binarySearch(14, array, 0, 6);
+	TEST_ASSERT_EQUAL(6 ,result);
+	TEST_ASSERT_EQUAL(14, array[result]);
+
+}
+
+
+/*
+ *	[2]	4	6	8	10	12	14	
+ *
+ */
+void test_binarySearch_given_array_7_elements_with_target_2_should_return_0(void)
+{
+	int array[] = {2,4,6,8,10,12,14};
+	int result;
+	
+	result = binarySearch(2, array, 0, 6);
+	TEST_ASSERT_EQUAL(0, result);
+	TEST_ASSERT_EQUAL(2, array[result]);
+}
+
+
+/*
+ *	2	4	6	8	10	12	14	[20]
+ *
+ */
+void test_binarySearch_given_array_7_elements_with_target_20_should_return_neg_1(void)
+{
+	int array[] = {2,4,6,8,10,12,14};
+	int result;
+	
+	result = binarySearch(20, array, 0, 6);
+	TEST_ASSERT_EQUAL(-1, result);
+}
+
+
